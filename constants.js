c7bb5c129a99d936144e0bbaaf0e9cd87d8122b8
Janki Dholariya <janki.dholariya@gmail.com>
2025-11-15 00:05:43 +0000

    update mochaHooks with colors

--- constants.js
+++ constants.js
@@ -5,3 +5,19 @@ const env = process.env;
 export const baseUrl = env.BASE_URL;
 export const userId = env.USER_ID;
 export const userSecretKey = env.USER_SECRET_KEY;
+
+export const colors = {
+  black: "\x1b[30m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+  white: "\x1b[37m",
+  resetColor: "\x1b[0m",
+};
+
+export const highlightLog = (color, message) => {
+  console.log(colors[color], message, colors.resetColor);
+};
